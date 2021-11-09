267445c8d4a89dc41f130058e691cdc5bb908c27
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-09 22:01:30 +0100

    Initial commit

---
+++ src/main/java/pl/internet_shop/InternetShopApplication.java
@@ -0,0 +1,13 @@
+package pl.internet_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/internet_shop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.internet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
