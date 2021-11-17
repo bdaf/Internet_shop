a34c467af1bb1ffb802a7e6e6418ffab885b7152
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-17 20:39:40 +0100

    Company class created

---
+++ src/main/java/pl/internet_shop/entity/Company.java
@@ -0,0 +1,26 @@
+package pl.internet_shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "IS_city")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_sequence")
+    @SequenceGenerator(name = "company_sequence", sequenceName = "company_sequence", allocationSize = 1)
+
+    @Column(name = "company_id", nullable = false)
+    private Long companyId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+}
