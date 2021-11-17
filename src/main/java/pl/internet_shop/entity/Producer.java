9d85a9edbbfa47ef12531d39a08bfb3d20d6b73e
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-17 22:05:45 +0100

    Producer class created

---
+++ src/main/java/pl/internet_shop/entity/Producer.java
@@ -0,0 +1,31 @@
+package pl.internet_shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "IS_producer")
+public class Producer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "producer_sequence")
+    @SequenceGenerator(name = "producer_sequence", sequenceName = "producer_sequence", allocationSize = 1)
+    @Column(name = "producer_id", nullable = false)
+    private Long producerId;
+
+    @Column(name = "name", nullable = false, length = 63)
+    private String name;
+
+    @Column(name = "surname", nullable = false, length = 63)
+    private String surname;
+
+    @Column(name = "nip", nullable = false, length = 15)
+    private String nip;
+}
