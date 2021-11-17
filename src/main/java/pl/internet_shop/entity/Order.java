a0c66492854aade20dc8fc838ed58555ab2ee559
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-17 22:27:12 +0100

    Order class created and linked

---
+++ src/main/java/pl/internet_shop/entity/Order.java
@@ -0,0 +1,27 @@
+package pl.internet_shop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "IS_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_sequence")
+    @SequenceGenerator(name = "order_sequence", sequenceName = "order_sequence", allocationSize = 1)
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @OneToMany
+    @JoinColumn(name = "order_id", referencedColumnName = "order_id")
+    private List<Product> products;
+}
