96301648fcd06caa7291ba0baa4344e452d19500
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-23 15:00:17 +0100

    Order test corrected

--- src/test/java/pl/internet_shop/repository/OrderRepositoryTest.java
+++ src/test/java/pl/internet_shop/repository/OrderRepositoryTest.java
@@ -35,7 +35,7 @@ class OrderRepositoryTest {
     private OrderRepository orderRepository;
 
     @Test
-    void saveOrderWithDeliveryAndCustomerThenPrintAllThenDeleteIt(){
+    void saveOrderWithDeliveryAndCustomerThenPrintAllThenDeleteIt() {
         Long amountOfDeliverers = delivererRepository.count();
         Long amountOfDeliveries = deliveryRepository.count();
         Long amountOfCustomers = customerRepository.count();
@@ -45,27 +45,34 @@ class OrderRepositoryTest {
 
         City city = CityRepository.getInstanceForTests();
         Address address = AddressRepository.getInstanceForTests();
+        address.setCity(city);
         Company company = CompanyRepository.getInstanceForTests();
-        Customer customer = CustomerRepository.getInstanceForTests();
         Deliverer deliverer = DelivererRepository.getInstanceForTests();
         Delivery delivery = DeliveryRepository.getInstanceForTests();
-        Order order = OrderRepository.getInstanceForTests();
-
-        address.setCity(city);
         delivery.setDeliverer(deliverer);
-        customer.setAddress(address);
-        customer.setCompany(company);
+        Order order = OrderRepository.getInstanceForTests();
+        Customer customer = Customer.builder()
+                .company(company)
+                .address(address)
+                .name("testCustomerName")
+                .surname("testCustomerSurname")
+                .email("testEmail@gmail.com")
+                .phoneNumber("testNumber")
+                .build();
+
+        customerRepository.save(customer);
+
         order.setCustomer(customer);
         order.setDelivery(delivery);
 
         orderRepository.save(order);
 
-        assertEquals(amountOfDeliverers+1, delivererRepository.count());
-        assertEquals(amountOfDeliveries+1, deliveryRepository.count());
-        assertEquals(amountOfCustomers+1, customerRepository.count());
-        assertEquals(amountOfCompanies+1, companyRepository.count());
-        assertEquals(amountOfAddressRecords+1,addressRepository.count());
-        assertEquals(amountOfCityRecords+1,cityRepository.count());
+        assertEquals(amountOfDeliverers + 1, delivererRepository.count());
+        assertEquals(amountOfDeliveries + 1, deliveryRepository.count());
+        assertEquals(amountOfCustomers + 1, customerRepository.count());
+        assertEquals(amountOfCompanies + 1, companyRepository.count());
+        assertEquals(amountOfAddressRecords + 1, addressRepository.count());
+        assertEquals(amountOfCityRecords + 1, cityRepository.count());
 
         List<Deliverer> deliverers = delivererRepository.findAll();
         System.out.println("deliverers = " + deliverers);
@@ -88,11 +95,11 @@ class OrderRepositoryTest {
         orderRepository.delete(order);
 
         assertEquals(amountOfDeliveries, deliveryRepository.count());
-        assertEquals(amountOfDeliverers+1, delivererRepository.count());
-        assertEquals(amountOfCustomers+1, customerRepository.count());
-        assertEquals(amountOfCompanies+1, companyRepository.count());
-        assertEquals(amountOfAddressRecords+1,addressRepository.count());
-        assertEquals(amountOfCityRecords+1,cityRepository.count());
+        assertEquals(amountOfDeliverers + 1, delivererRepository.count());
+        assertEquals(amountOfCustomers + 1, customerRepository.count());
+        assertEquals(amountOfCompanies + 1, companyRepository.count());
+        assertEquals(amountOfAddressRecords + 1, addressRepository.count());
+        assertEquals(amountOfCityRecords + 1, cityRepository.count());
 
         delivererRepository.delete(deliverer);
         customerRepository.delete(customer);
@@ -100,10 +107,10 @@ class OrderRepositoryTest {
         assertEquals(amountOfDeliverers, delivererRepository.count());
         assertEquals(amountOfCustomers, customerRepository.count());
         assertEquals(amountOfCompanies, companyRepository.count());
-        assertEquals(amountOfAddressRecords,addressRepository.count());
-        assertEquals(amountOfCityRecords+1,cityRepository.count());
+        assertEquals(amountOfAddressRecords, addressRepository.count());
+        assertEquals(amountOfCityRecords + 1, cityRepository.count());
 
         cityRepository.delete(city);
-        assertEquals(amountOfCityRecords,cityRepository.count());
+        assertEquals(amountOfCityRecords, cityRepository.count());
     }
 }
