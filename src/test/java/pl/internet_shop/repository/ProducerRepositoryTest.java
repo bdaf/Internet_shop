c563d194127a3840b5b6e0be28a8b420fe4c8733
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-11-23 02:11:53 +0100

    Producer class is basic tested with save and delete

---
+++ src/test/java/pl/internet_shop/repository/ProducerRepositoryTest.java
@@ -0,0 +1,35 @@
+package pl.internet_shop.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.internet_shop.entity.Producer;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ProducerRepositoryTest {
+
+    @Autowired
+    ProducerRepository producerRepository;
+
+    @Test
+    void saveProducerThenPrintAllThenDeleteIt(){
+        Long amountOfProducers = producerRepository.count();
+
+        Producer producer = Producer.builder()
+                .name("TestName").surname("TestSurname")
+                .nip("testNip123").build();
+
+        producerRepository.save(producer);
+        assertEquals(amountOfProducers+1, producerRepository.count());
+
+        List<Producer> producers = producerRepository.findAll();
+        System.out.println("producers = " + producers);
+
+        producerRepository.delete(producer);
+        assertEquals(amountOfProducers, producerRepository.count());
+    }
+}
