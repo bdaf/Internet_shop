43936a4e1c8c969d58a532ce95678bc6b8f60519
Patryk Osak <pionchaoo@gmail.com>
2021-11-29 21:22:46 +0100

    add contact and about us pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
+import About from './pages/About/About';
 
 import './App.css';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/detail/:id" element={<ProductDetails />} />
---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="container">
+            <div className="card mt-4">
+                <div className="card-body">
+                    <h2>About us</h2>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About
