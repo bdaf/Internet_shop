b986d29a6b55ad8f36f24dce045719709b77e663
Rafał Zaborowski <raffik2000@gmail.com>
2021-12-04 18:37:13 +0100

    Add feedback when add product

--- frontend/src/pages/EmployeePanel/components/Products/AddProduct.js
+++ frontend/src/pages/EmployeePanel/components/Products/AddProduct.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Col, Row, FloatingLabel, Form, Button } from "react-bootstrap";
+import { useNavigate } from 'react-router'
+import { Col, Row, FloatingLabel, Form, Button, Alert } from "react-bootstrap";
 import Select from 'react-select';
 
 const AddProduct = () => {
+    const navigate = useNavigate();
+
     const [name, setName] = useState('')
     const [description, setDescription] = useState('')
     const [price, setPrice] = useState(0)
@@ -14,28 +17,25 @@ const AddProduct = () => {
     const [optionsCategory, setOptionsCategory] = useState([])
     const [optionsProducer, setOptionsProducer] = useState([])
 
-
+    const [isError, setIsError] = useState(false)
 
     const fetchDate = async () => {
         await axios.get("http://localhost:8888/api/categories").then((response) => {
             const optionsC = response.data.map((c) => {
-                const opt = { value: c.categoryId, label: c.name}
+                const opt = { value: c.categoryId, label: c.name }
                 return opt
             })
             setOptionsCategory(optionsC)
         })
         await axios.get("http://localhost:8888/api/producers").then((response) => {
             const optionsP = response.data.map((p) => {
-                const opt = { value: p.nip, label: p.nameOfCompany}
+                const opt = { value: p.nip, label: p.nameOfCompany }
                 return opt
             })
             setOptionsProducer(optionsP)
-
         })
 
     }
-
-
 
     useEffect(() => {
         fetchDate()
@@ -44,6 +44,8 @@ const AddProduct = () => {
     const addProductHandler = async (e) => {
         e.preventDefault();
 
+        setIsError(false)
+
         const newProduct =
         {
             name: name,
@@ -51,7 +53,7 @@ const AddProduct = () => {
             price: price,
             amount: amount,
             category: {
-                 name: category,
+                name: category,
                 discounts: []
             },
             producer: {
@@ -61,13 +63,25 @@ const AddProduct = () => {
         }
 
         await axios.post(`http://localhost:8888/api/products/save`, newProduct).then((response) => {
-            console.log(response.data)
-        })
+            console.log(response)
+            if (response.status === 200)
+                navigate(`/detail/${response.data.productId}`)
+            else
+                setIsError(true)
+        }).catch(
+            setIsError(true)
+        )
     }
 
     return (
         <div className="m-3">
             <Form onSubmit={(e) => addProductHandler(e)}>
+                {isError ?
+                    <Alert variant="danger">
+                        Nie udało się dodać nowego przedmiotu!
+                    </Alert>
+                    : null}
+
                 <Row className="mb-3">
                     <Form.Group as={Col} xs={12} md={6} controlId="formGridName">
                         <FloatingLabel controlId="floatingPassword" label="Nazwa produktu">
@@ -103,9 +117,12 @@ const AddProduct = () => {
                         <Select onChange={(e) => setProducer(e)} options={optionsProducer} placeholder="Producent" />
                     </Col>
                 </Row>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <div className="d-flex justify-content-end">
+
+                    <Button className="ps-4 pe-4" variant="outline-primary" type="submit">
+                        Dodaj nowy produkt
+                    </Button>
+                </div>
             </Form>
         </div>
     );
