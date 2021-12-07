57bcce44b7cc6779f9e1a925060a6d9867ed9a78
Rafał Zaborowski <raffik2000@gmail.com>
2021-12-07 15:04:27 +0100

    Add filters

---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,50 @@
+import { Row, Col, FloatingLabel, Form, InputGroup, FormControl } from "react-bootstrap";
+
+const Filter = (props) => {
+    return (
+        <div className="m-3">
+            <h4>Filtry</h4>
+            <hr />
+            <Form>
+                <Row className="mb-2">
+                    <Col xs={12} md={6}>
+                        <FloatingLabel className="mb-1" controlId="floatingPassword" label="Nazwa">
+                            <Form.Control type="text" placeholder="Nazwa" onChange={(e) => props.onSetName(e.target.value)} value={props.name}/>
+                        </FloatingLabel>
+                    </Col>
+                </Row>
+                <Row className="mb-2">
+                    <h5>Cena</h5>
+                    <Col xs={4}>
+                        <InputGroup>
+                            <FormControl
+                                placeholder="Od"
+                                aria-describedby="basic-addon2"
+                                type="number"
+                                onChange={(e) => props.onSetFromPirice(e.target.value)}
+                                value={props.fromPrice}
+                            />
+                            <InputGroup.Text id="basic-addon2">PLN</InputGroup.Text>
+                        </InputGroup>
+                    </Col>
+                    ---
+                    <Col xs={4}>
+                        <InputGroup>
+                            <FormControl
+                                placeholder="Do"
+                                aria-describedby="basic-addon2"
+                                type="number"
+                                onChange={(e) => props.onSetToPirice(e.target.value)}
+                                value={props.toPrice}
+                            />
+                            <InputGroup.Text id="basic-addon2">PLN</InputGroup.Text>
+                        </InputGroup>
+                    </Col>
+                </Row>
+            </Form>
+            <hr />
+        </div>
+    );
+}
+
+export default Filter
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -5,16 +5,25 @@ import Navbar from "../../components/Navbar/Navbar";
 import Offer from "./components/Offer/Offer";
 import Product from "./components/Product/Product";
 import Footer from "../../components/Footer/Footer";
+import Filter from '../../components/Filter/Filter';
 
-
+import styles from './Home.module.css'
+import LoadingIcon from '../../components/LoadingIcon/LoadingIcon';
 
 const Home = () => {
-    const [products, setProducts] = useState(null)
+    const [allProducts, setAllProducts] = useState(null)
+    const [filterProducts, setFilterProducts] = useState(null)
     const [loading, setLoading] = useState(false)
+
+    const [isFiler, setIsFiler] = useState(false)
+    const [name, setName] = useState('')
+    const [fromPrice, setFromPrice] = useState('')
+    const [toPrice, setToPrice] = useState('')
 
     const fetchProducts = async () => {
         await axios.get("http://localhost:8888/api/products").then((response) => {
-            setProducts(response.data)
+            setAllProducts(response.data)
+            setFilterProducts(response.data)
         })
         setLoading(true)
     }
@@ -23,18 +32,36 @@ const Home = () => {
         fetchProducts()
     }, [])
 
+    const actProduct = () => {
+        let newActualyProduct = allProducts;
+        if (fromPrice || toPrice) {
+            newActualyProduct = [...newActualyProduct]
+                .filter(product => (product.price >= (fromPrice ? (+fromPrice) : 0)) && (product.price <= (toPrice ? (+toPrice) : 100000000000000)))
+        }
+        if (name) {
+            newActualyProduct = [...newActualyProduct]
+                .filter(product => product.name.toLowerCase()
+                    .includes(name.toLowerCase()))
+        }
+        return newActualyProduct
+    }
+
+    useEffect(() => {
+        setFilterProducts(actProduct())
+    }, [name, allProducts, fromPrice, toPrice])
+
     const searchHandler = (term) => {
         if (!term)
             fetchProducts()
         else {
-            const filterProducts = [...products]
-                .filter(product => product.name.toLowerCase()
-                    .includes(term.toLowerCase()))
-            setProducts(filterProducts);
+            // const filterProducts = [...allProducts]
+            //     .filter(product => product.name.toLowerCase()
+            //         .includes(term.toLowerCase()))
+            // setAllProducts(filterProducts);
         }
     }
 
-    const contextProduct = !loading ? <p>loading...</p> : products.map((product) =>
+    const contextProduct = !loading ? <LoadingIcon /> : filterProducts.map((product) =>
         <Col md="auto">
             <Product
                 key={product.productId}
@@ -54,13 +81,27 @@ const Home = () => {
                 <Offer />
             </div>
             <div className="m-4">
-                <h1 className=" justify-content-md-center">Produkty</h1>
+                <Row>
+                    <Col xs={11}><h1>Produkty</h1></Col>
+                    <Col className="d-flex justify-content-end" xs={1}>
+                        <i onClick={() => setIsFiler((prev) => !prev)} className={`bi bi-filter ${isFiler ? styles.filterActive : styles.filter}`}></i>
+                    </Col>
+                </Row>
                 <hr />
+                {isFiler &&
+                    <Filter
+                        name={name}
+                        fromPrice={fromPrice}
+                        toPrice={toPrice}
+                        onSetName={setName}
+                        onSetFromPirice={setFromPrice}
+                        onSetToPirice={setToPrice}
+                    />}
                 <Row className="justify-content-md-center">
                     {contextProduct}
                 </Row>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
