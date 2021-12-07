fd0e6d41fe749c65375505d5c1b7f6c90f76774e
Rafał Zaborowski <raffik2000@gmail.com>
2021-12-07 02:53:51 +0100

    make bigger logo

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ const Navbars = (props) => {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand>NUIT</Navbar.Brand>
+                <Navbar.Brand> <h2>NU<span style={{ color: "red" }} >IT</span></h2></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -48,16 +48,19 @@ const Navbars = (props) => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link><NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/"> Strona główna</NavLink></Nav.Link>
-                        {(authCtx.role === "WORKER" || authCtx.role === "ADMIN") && <Nav.Link href="/panel">Panel</Nav.Link>}
+                        <Nav.Link><NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/">Strona główna</NavLink></Nav.Link>
+                        {(authCtx.role === "WORKER" || authCtx.role === "ADMIN") &&
+                            <Nav.Link >
+                                <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/panel">Panel</NavLink>
+                            </Nav.Link>}
                         <Nav.Link>
-                            <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/about"> O nas </NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/about">O nas</NavLink>
                         </Nav.Link>
                         <Nav.Link>
                             <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/contact">Kontakt</NavLink>
                         </Nav.Link>
                         <Nav.Link>
-                        <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/faq">FAQ</NavLink>
+                            <NavLink className={({ isActive }) => (isActive ? `${styles.menuItemActive}` : `${styles.menuItem}`)} to="/faq">FAQ</NavLink>
                         </Nav.Link>
                     </Nav>
                     {props.search ? (<>
