c1034a47583cc867d75301de9f74f10fc9f04d6a
Rafał Zaborowski <raffik2000@gmail.com>
2021-12-08 23:27:06 +0100

    add and remove  product with cart

--- frontend/src/store/cart-context.js
+++ frontend/src/store/cart-context.js
@@ -17,7 +17,7 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
         const productCart = {
-            id: action.item.productId,
+            productId: action.item.productId,
             name: action.item.name,
             description: action.item.description,
             price: action.item.price,
@@ -27,11 +27,44 @@ const cartReducer = (state, action) => {
             gallery: action.item.gallery
         }
 
-        const updatedItems = state.items.concat(productCart)
+        let updatedItems;
         const updatedTotalAmount = state.totalAmount + productCart.amount * productCart.price
 
-        console.log(updatedItems)
-        console.log(updatedTotalAmount)
+        const exisitngCartItemsIndex = state.items.findIndex((item) => item.productId === productCart.productId)
+        const exisitngItem = state.items[exisitngCartItemsIndex]
+
+        if (exisitngItem) {
+            const upadatedItem = {
+                ...exisitngItem,
+                amount: exisitngItem.amount + productCart.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[exisitngCartItemsIndex] = upadatedItem
+        } else {
+            updatedItems = state.items.concat(productCart)
+        }
+        // console.log(updatedItems)
+        // console.log(updatedTotalAmount)
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    else if (action.type === 'REMOVE') {
+        const exisitngCartItemsIndex = state.items.findIndex((item) => item.productId === action.productId)
+        const exisitngItem = state.items[exisitngCartItemsIndex]
+        const updatedTotalAmount = state.totalAmount - exisitngItem.price;
+
+        let updatedItems
+        if (exisitngItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.productId !== action.productId)
+        } else {
+            const upadatedItem = { ...exisitngItem, amount: exisitngItem.amount - 1 }
+            updatedItems = [...state.items]
+            updatedItems[exisitngCartItemsIndex] = upadatedItem
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -48,7 +81,7 @@ export const CartContextProvider = (props) => {
     }
 
     const removeItemFromCartHandler = id => {
-        dispatchCartAction({type: 'REMOVE', id: id})
+        dispatchCartAction({type: 'REMOVE', productId: id})
     }
 
     const contextValue = {
